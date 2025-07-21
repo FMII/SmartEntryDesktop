131a47edb548eaabbfb37b93e47073ec479aba07
misrael97 <moromoroni123@gmail.com>
2025-07-20 18:36:00 -0600

    creacion vistas Horario e historial

---
+++ src/app/pages/historial-asistencia/historial-asistencia.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-historial-asistencia',
+  standalone: true,
+  imports: [],
+  templateUrl: './historial-asistencia.component.html',
+  styleUrl: './historial-asistencia.component.css'
+})
+export class HistorialAsistenciaComponent {
+
+}
---
+++ src/app/pages/horario-docentes/horario-docentes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-horario-docentes',
+  standalone: true,
+  imports: [],
+  templateUrl: './horario-docentes.component.html',
+  styleUrl: './horario-docentes.component.css'
+})
+export class HorarioDocentesComponent {
+
+}
